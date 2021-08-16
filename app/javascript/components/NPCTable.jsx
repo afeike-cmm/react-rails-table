2e455cb2da890a257c9acca9fd5941b4246dfaa6
Alex Feike <afeike@covermymeds.com>
2021-08-16 12:57:02 -0400

    remove unused code

--- app/javascript/components/NPCTable.jsx
+++ app/javascript/components/NPCTable.jsx
@@ -105,19 +105,6 @@ const NPCTable = () => {
         []
     )
 
-    // const {
-    //     getTableProps,
-    //     getTableBodyProps,
-    //     headerGroups,
-    //     prepareRow,
-    //     rows,  
-    // } = useTable(
-    //     { 
-    //         columns, 
-    //         data: data,
-    //     }
-    // )
-
     return (
         <>
             <div>
@@ -132,29 +119,6 @@ const NPCTable = () => {
                     placeholder={"Search last name"}
                 />
             </div>
-            {/* <table {...getTableProps()}>
-                <thead>
-                    {headerGroups.map(headerGroup => (
-                        <tr {...headerGroup.getHeaderGroupProps()}>
-                            {headerGroup.headers.map(column => (
-                                <th {...column.getHeaderProps()}>{column.render("Header")}</th>
-                            ))}
-                        </tr>
-                    ))}
-                </thead>
-                <tbody {...getTableBodyProps()}>
-                    {rows.map((row, i) => {
-                        prepareRow(row);
-                        return (
-                            <tr {...row.getRowProps()}>
-                                {row.cells.map(cell => {
-                                    return <td {...cell.getCellProps()}>{cell.render("Cell")}</td>
-                                })}
-                            </tr>
-                        );
-                    })}
-                </tbody>
-            </table> */}
             <div className="table-responsive">
                 <Table columns={columns} data={data} />
             </div>
